7eac4eed5d57b3ba8b7d55787e8a8ca1e7f12ffc
lionelMaquet <lionel.maquet@gmail.com>
2019-02-17 18:39:53 +0100

    Optimized

--- MEDIUM/3D Surface Area/main.js
+++ MEDIUM/3D Surface Area/main.js
@@ -1,13 +1,13 @@
-// A = [[1,2,3][4,2,1],[5,3,4]] ...
+// input is A
+// structure of A = [[1,2,3][4,2,1],[5,3,4]] ...
 
 function surfaceArea(A) {
 
-
-  let sum = 0;
+  let area = 0;
   let rows = A.length;
   let cols = A[0].length;
-  let sides = [[0,1],[0,-1],[1,0],[-1,0]];
-  console.log(rows,cols)
+  let sides = [[0,1],[0,-1],[1,0],[-1,0]]; // adds these numbers to i and j to check the numbers on each cell aside from the current one
+
   for (let i = 0; i < rows ; i++) {
 
     for (let j = 0; j < cols ; j++) {
@@ -17,34 +17,35 @@ function surfaceArea(A) {
         let currentSide = A[i][j];
         let checkedSide;
 
-        if (i + side[0] < 0 || i + side[0] >= A.length || j + side[1] < 0 || j + side[1] >= A[0].length ) {
-          // if the side is an edge
-          console.log(currentSide)
-          sum+= currentSide
+        if (i + side[0] < 0 || i + side[0] >= A.length || j + side[1] < 0 || j + side[1] >= A[0].length ) { // if the side checked is an edge
+
+          area += currentSide
+
         } else {
+
           checkedSide = A[(i + side[0])][j + (side[1])];
+
             if (checkedSide < currentSide) {
-              console.log(currentSide - checkedSide)
-              sum += (currentSide - checkedSide)
+
+              area += (currentSide - checkedSide);
+
             }
+
           }
 
         }
 
-      sum+= 2// for the top and bottom sides
+      area += 2// for the top and bottom sides
+
       }
-
-
 
     }
 
+    return area
 
-    return sum
   }
 
 
-
-
-
-
-console.log(surfaceArea([ [ 1, 3, 4 ], [ 2, 2, 3 ], [ 1, 2, 4 ] ]))
+// Example
+// console.log(surfaceArea([ [ 1, 3, 4 ], [ 2, 2, 3 ], [ 1, 2, 4 ] ]))
+// Output area must be 60
